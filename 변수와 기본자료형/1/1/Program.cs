85c0a84431faf3a425722e00e2410aedb17da1c5
Nakkyoo <87404074+Nakkyoo@users.noreply.github.com>
2021-09-13 21:43:09 +0900

    Add files via upload

---
+++ 변수와 기본자료형/1/1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age = 12;
+            Console.WriteLine(age);
+        }
+    }
+}
