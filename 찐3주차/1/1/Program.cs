1907de0d576faece4fd64e92aa792ab11868b5a8
Nakkyoo <87404074+Nakkyoo@users.noreply.github.com>
2021-09-14 21:40:58 +0900

    Add files via upload

---
+++ 찐3주차/1/1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte int1 = 0;
+            float char1 = int1;
+            Console.Write(char1);
+        }
+    }
+}
