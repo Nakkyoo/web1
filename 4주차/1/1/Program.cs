1fa012654f45042598490c1d80f5ee47a9ff5a0a
Nakkyoo <87404074+Nakkyoo@users.noreply.github.com>
2021-09-16 22:01:27 +0900

    Add files via upload

---
+++ 4주차/1/1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("상우네 계산기");
+            Console.WriteLine("첫 번째 숫자 입력:");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("두 번째 숫자 입력:");
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = 0;
+            while (number3 == 0) {
+                Console.WriteLine("원하는 연산자 입력 + - * / :");
+                string op = Console.ReadLine();
+                if (op == "+")
+                {
+                    Console.WriteLine($"{number1} {op} {number2} = {number1 + number2}");
+                    break;
+                }
+                if (op == "-")
+                {
+                    Console.WriteLine($"{number1} {op} {number2} = {number1 - number2}");
+                    break;
+                }
+                if (op == "*")
+                {
+                    Console.WriteLine($"{number1} {op} {number2} = {number1 * number2}");
+                    break;
+                }
+                if (op == "/")
+                {
+                    Console.WriteLine($"{number1} {op} {number2} = {number1 / number2}");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine($"정확한 연산자를 입력해주십시오.");
+                    continue;
+                }
+                
+            };
+        }
+    }
+}
---
+++ 4주차/2/2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            if (3*6 != 18 || ++num == 11)
+            {
+                num++;
+            }
+            if (12 + 2 == 14 && num++ == 12)
+            {
+                Console.WriteLine("12가 맞았어!");
+            }
+        }
+    }
+}
