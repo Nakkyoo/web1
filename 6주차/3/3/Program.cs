58b66806d41fa6a80cb4ab17cfafce3bf16a1cd1
Nakkyoo <87404074+Nakkyoo@users.noreply.github.com>
2021-09-17 21:55:46 +0900

    Add files via upload

---
+++ 6주차/3/3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("상우네 Switch/Case/Default계산기");
+            Console.WriteLine("첫번째 정수형 숫자를 입력해주십시오 좆같은거 입력하면 예외처리하게 빠진답니다.");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("두번째 숫자를 입력해주십시오  좆같은거 입력하면 예외처리하게 빠진답니다.");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("원하는 연산을 + - * / 입력해주십시오 좆같은거 입력하면 예외처리하게 빠진답니다.");
+            char op = char.Parse(Console.ReadLine());
+            switch(op)
+            {
+                case '+':
+                    Console.WriteLine($"{number1}+{number2} = {number1 + number2}");
+                    break;
+                case '-':
+                    Console.WriteLine($"{number1}-{number2} = {number1 - number2}");
+                    break;
+                case '*':
+                    Console.WriteLine($"{number1}*{number2} = {number1 * number2}");
+                    break;
+                case '/':
+                    Console.WriteLine($"{number1}/{number2}  한 결과 몫은 {number1 / number2} 나머지는 {number1 % number2}");
+                    break;
+                default:
+                    Console.WriteLine("븅신아 연산자 똑바로 입력하라고");
+                    break;
+            }
+        }
+    }
+}
---
+++ 8주차/1/1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1부터 n까지의 정수의 합 구하~기");
+            Console.WriteLine("원하는 n 값을 입력해 주십시오");
+            int number = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < number; ++i)
+            {
+                Console.WriteLine($"{result}+{i} = {result + i + 1}");
+                result += i+1;
+                if(i+1 == number)
+                {
+                    Console.WriteLine($"계산 끝, 최총 SUM 값 {result}");
+                }
+            }
+        }
+    }
+}
